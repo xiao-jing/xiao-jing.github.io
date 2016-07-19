d791a4e77bd8a9c658a59870ae321d558dbddb82
xiao-jing <xiao-jing@outlook.com>
2016-07-19 12:59:38 +0800

    Site updated: 2016-07-19 12:59:28

---
+++ Sance/js/main.js
@@ -0,0 +1,12 @@
+$(window).load(function(){
+	$(".J_intro").typed({
+		strings:["Hello, I am Jingzi, a smile Front-end girl ~"],
+		typeSpeed:80
+	});
+});
+$(window).load(function(){
+	$(".typed").typed({
+		strings:["Hello, I am Jingzi, a smile girl ~"],
+		typeSpeed:80
+	});
+});
--- demo/a-smile-girl/js/main.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function(){
-	$(".typed").typed({
-		strings:["Hello, I am Jingzi, a smile girl ~"],
-		typeSpeed:80
-	});
-});
